54c01f6fb10260abb82ebc22e06438f8369a2ddb
astrapi69 <asterios.raptis@gmx.net>
2015-12-12 14:12:07 +0100

    Mapper updated after refactoring.

--- scheduler-system-domain/src/main/java/de/alpharogroup/scheduler/system/mapper/AppointmentsMapper.java
+++ scheduler-system-domain/src/main/java/de/alpharogroup/scheduler/system/mapper/AppointmentsMapper.java
@@ -1,9 +1,9 @@
 package de.alpharogroup.scheduler.system.mapper;
 
-import de.alpharogroup.db.entitymapper.AbstractEntityBOMapper;
+import de.alpharogroup.db.entitymapper.AbstractEntityDOMapper;
 import de.alpharogroup.scheduler.system.domain.Appointment;
 import de.alpharogroup.scheduler.system.entities.Appointments;
 
-public class AppointmentsMapper extends AbstractEntityBOMapper<Appointments, Appointment> {
+public class AppointmentsMapper extends AbstractEntityDOMapper<Appointments, Appointment> {
 
 }
