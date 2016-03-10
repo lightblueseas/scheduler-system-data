89aa55d46db45208948db7b387dff47532c1b8ee
astrapi69 <asterios.raptis@gmx.net>
2016-03-10 16:32:59 +0100

    new profile to create an empty db if not exist.

--- scheduler-system-init/src/main/java/de/alpharogroup/db/init/InitializeDatabase.java
+++ scheduler-system-init/src/main/java/de/alpharogroup/db/init/InitializeDatabase.java
@@ -34,6 +34,9 @@ public class InitializeDatabase
 	public static void main(final String[] args) throws IOException, ClassNotFoundException, SQLException
 	{
 		final Properties dbProperties = PropertiesExtensions.loadProperties("jdbc.properties");
+		if(args != null && 0< args.length ) {
+			dbProperties.setProperty(AbstractDatabaseInitialization.JDBC_CREATE_DB_PROCESS_KEY, args[0]);
+		}
 		new DatabaseInitialization(dbProperties).initializeDatabase();
 	}
 
